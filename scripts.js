e36f14b80d8fa53f7976f963f9a1f28b6e4f78df
Joanna Buchała <joanna.b2@op.pl>
2018-05-09 01:35:18 +0200

    add images

--- scripts.js
+++ scripts.js
@@ -3,22 +3,29 @@ var movies = [
 		id: 1,
 		title: 'Harry Potter',
 		desc: 'Movie about some wizard', 
-		src: "./harry.jpg alt=potter"
+		src: "./potter.jpg",
+		alt: "potter"
 	},
 	{
 		id: 2,
 		title: 'Lion King',
-		desc: 'Everyone cry on this'
+		desc: 'Everyone cry on this',
+		src: "./king.jpg",
+		alt: "king"
 	},
 	{
 		id: 3,
 		title: 'Fight Club',
-		desc: 'I should not talk about this'
+		desc: 'I should not talk about this',
+		src: "./fight.jpg",
+		alt: "club"
 	},
 	{
 		id: 4,
 		title: 'A Clockwork Orange',
-		desc: 'History about charming Alex DeLarge'
+		desc: 'History about charming Alex DeLarge',
+		src: "./clock.jpg",
+		alt: "orange"
 	}
 ];
 
@@ -26,8 +33,8 @@ var moviesElements = movies.map(function(movie){
 	return React.createElement('li', {key: movie.id},
 		React.createElement('h2', {}, movie.title),
 		React.createElement('p', {}, movie.desc),
-		React.createElement('img', {key: movie.src})
-		);
+		React.createElement('img', {src: movie.src, alt: movie.alt}
+		));
 });
 
 var element = 
